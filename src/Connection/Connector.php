591fe44a457837f7c533d17c33d8954165f8fb41
Patrick Dawkins <pjcdawkins@gmail.com>
2015-03-26 17:34:20 +0000

    Release v0.1.1

--- src/Connection/Connector.php
+++ src/Connection/Connector.php
@@ -48,7 +48,7 @@ class Connector implements ConnectorInterface
      */
     public function __construct(array $config = [], SessionInterface $session = null)
     {
-        $version = '0.1.0';
+        $version = '0.1.x';
         $url = 'https://github.com/platformsh/platformsh-client-php';
 
         $defaults = [
