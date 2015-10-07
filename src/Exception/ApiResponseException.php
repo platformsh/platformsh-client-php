d01266f0cda663eab9dfde308f9aed4b75e9d368
Patrick Dawkins <pjcdawkins@gmail.com>
2015-10-07 12:46:12 +0100

    Expose ApiResponseException::getErrorDetails() so the CLI can reuse the logic

--- src/Exception/ApiResponseException.php
+++ src/Exception/ApiResponseException.php
@@ -26,27 +26,45 @@ class ApiResponseException extends BadResponseException
             return $e;
         }
 
-        $originalMessage = $e->getMessage();
-        $message = $originalMessage;
+        // Re-create the exception to alter the message.
+        $details = self::getErrorDetails($response);
+        if (!empty($details)) {
+            $className = get_class($e);
+            $e = new $className($e->getMessage() . $details, $e->getRequest(), $e->getResponse());
+        }
 
+        return $e;
+    }
+
+    /**
+     * Get more details from the response body, to add to error messages.
+     *
+     * @param ResponseInterface $response
+     *
+     * @return string
+     */
+    public static function getErrorDetails(ResponseInterface $response)
+    {
         $responseInfoProperties = [
-          // Platform.sh API errors.
+            // Platform.sh API errors.
           'message',
           'detail',
-          // RESTful module errors.
+            // RESTful module errors.
           'title',
           'type',
-          // OAuth2 errors.
+            // OAuth2 errors.
           'error',
           'error_description',
         ];
+
+        $details = '';
 
         try {
             $response->getBody()->seek(0);
             $json = $response->json();
             foreach ($responseInfoProperties as $property) {
                 if (!empty($json[$property])) {
-                    $message .= " [$property] " . implode('; ', (array) $json[$property]);
+                    $details .= " \n [$property] " . implode('; ', (array) $json[$property]);
                 }
             }
         } catch (ParseException $parseException) {
@@ -54,20 +72,14 @@ class ApiResponseException extends BadResponseException
             $response->getBody()->seek(0);
             $body = $response->getBody()->getContents();
             if ($body) {
-                $message .= ' [extra] Non-JSON response body';
-                $message .= ' [body] ' . $body;
+                $details .= " \n [extra] Non-JSON response body";
+                $details .= " \n [body] " . $body;
             }
             else {
-                $message .= ' [extra] Empty response body';
+                $details .= " \n [extra] Empty response body";
             }
         }
 
-        // Re-create the exception to alter the message.
-        if ($message !== $originalMessage) {
-            $className = get_class($e);
-            $e = new $className($message, $e->getRequest(), $e->getResponse());
-        }
-
-        return $e;
+        return $details;
     }
 }
