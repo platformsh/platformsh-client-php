05e7d36361991e3e5801fdc07f2726dbd6505bbf
Patrick Dawkins <pjcdawkins@gmail.com>
2017-11-29 12:03:52 +0000

    Implement Git Data API

---
+++ src/Model/Git/Tree.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Platformsh\Client\Model\Git;
+
+use GuzzleHttp\Client;
+use Platformsh\Client\Model\Project;
+use Platformsh\Client\Model\Resource;
+
+/**
+ * Git tree resource.
+ *
+ * @property-read string $id
+ * @property-read string $sha
+ * @property-read array  $tree
+ */
+class Tree extends Resource
+{
+    /**
+     * Get the Tree object for an SHA hash.
+     *
+     * @param string $sha
+     * @param string $baseUrl
+     * @param Client $client
+     *
+     * @return static|false
+     */
+    public static function fromSha($sha, $baseUrl, Client $client)
+    {
+        $url = Project::getProjectBaseFromUrl($baseUrl) . '/git/trees';
+
+        return static::get($sha, $url, $client);
+    }
+
+    /**
+     * Get an object in this tree.
+     *
+     * @param string $path The path to an object in the tree.
+     *
+     * @return Blob|Tree|false
+     *   A Blob or Tree object, or false if the object does not exist.
+     */
+    public function getObject($path)
+    {
+        $data = false;
+        foreach ($this->tree as $objectData) {
+            if ($objectData['path'] === $path) {
+                $data = $objectData;
+                break;
+            }
+        }
+        if ($data === false) {
+            return false;
+        }
+
+        if ($data['type'] === 'blob') {
+            return Blob::fromSha($data['sha'], $this->getUri(), $this->client);
+        } elseif ($data['type'] === 'tree') {
+            return Tree::fromSha($data['sha'], $this->getUri(), $this->client);
+        }
+
+        throw new \RuntimeException('Unrecognised object type: ' . $data['type']);
+    }
+
+    /**
+     * Get an object recursively in this tree.
+     *
+     * @param string $path
+     *
+     * @return Blob|Tree|false
+     */
+    private function getObjectRecursive($path)
+    {
+        if (strpos($path, '/') === false && strpos($path, '\\') !== false) {
+            $path = str_replace('\\', '/', $path);
+        }
+        $parts = explode('/', trim($path, '/'));
+        $object = false;
+        $tree = $this;
+        while ($part = array_shift($parts)) {
+            $object = $tree->getObject($part);
+            if ($object instanceof Tree) {
+                $tree = $object;
+            } else {
+                return $object;
+            }
+        }
+
+        return $object;
+    }
+
+    /**
+     * Get a Blob (file) inside this tree.
+     *
+     * @param string $path
+     *
+     * @return Blob|false
+     */
+    public function getBlob($path)
+    {
+        $object = $this->getObjectRecursive($path);
+        if ($object instanceof Blob) {
+            return $object;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get a Tree (directory) inside this tree.
+     *
+     * @param string $path
+     *
+     * @return Tree|false
+     */
+    public function getTree($path)
+    {
+        $object = $this->getObjectRecursive($path);
+        if ($object instanceof Tree) {
+            return $object;
+        }
+
+        return false;
+    }
+}
--- src/Model/Project.php
+++ src/Model/Project.php
@@ -2,6 +2,8 @@
 
 namespace Platformsh\Client\Model;
 
+use function GuzzleHttp\Psr7\uri_for;
+
 /**
  * A Platform.sh project.
  *
@@ -375,5 +377,21 @@ class Project extends Resource
         $options = ['key' => $key, 'certificate' => $certificate, 'chain' => $chain];
 
         return Certificate::create($options, $this->getUri() . '/certificates', $this->client);
+    }
+
+    /**
+     * Find the project base URL from another project resource's URL.
+     *
+     * @param string $url
+     *
+     * @return string
+     */
+    public static function getProjectBaseFromUrl($url)
+    {
+        if (preg_match('#/api/projects/([^/]+)#', $url, $matches)) {
+            return uri_for($url)->withPath('/api/projects/' . $matches[1])->__toString();
+        }
+
+        throw new \RuntimeException('Failed to find project ID from URL: ' . $url);
     }
 }
