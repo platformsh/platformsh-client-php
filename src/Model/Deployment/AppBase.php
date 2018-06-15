c69c018f02a06d2c2746e4f4fac9c8bbe7334713
Patrick Dawkins <pjcdawkins@gmail.com>
2018-06-15 14:38:58 +0100

    Add missing property info to AppBase

--- src/Model/Deployment/AppBase.php
+++ src/Model/Deployment/AppBase.php
@@ -7,10 +7,18 @@ use Platformsh\Client\DataStructure\ReadOnlyStructureTrait;
 /**
  * An application.
  *
- * @property-read string $name
- * @property-read string $type
- * @property-read array  $runtime
- * @property-read array  $preflight
+ * @property-read string      $name
+ * @property-read string      $type
+ *
+ * @property-read array       $access
+ * @property-read int         $disk
+ * @property-read array       $mounts
+ * @property-read array       $preflight
+ * @property-read array       $relationships
+ * @property-read array       $runtime
+ * @property-read string      $size
+ * @property-read string|null $timezone
+ * @property-read array       $variables
  */
 abstract class AppBase
 {
