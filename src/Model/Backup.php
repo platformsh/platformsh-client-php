cb7e7479eeccf2a036be9d3b3d29c1548e56fdcb
Patrick Dawkins <pjcdawkins@gmail.com>
2018-09-06 10:59:08 +0100

    Add support for viewing and modifying backup policies

--- src/Model/Backup.php
+++ src/Model/Backup.php
@@ -2,9 +2,6 @@
 
 namespace Platformsh\Client\Model;
 
-use GuzzleHttp\ClientInterface;
-use function GuzzleHttp\Psr7\uri_for;
-
 /**
  * An environment backup.
  *
@@ -22,30 +19,4 @@ class Backup extends ApiResourceBase
 {
     const STATUS_CREATED = 'CREATED';
     const STATUS_DELETED = 'DELETED';
-
-    /**
-     * Restore this backup.
-     *
-     * @param string|null $environment The environment ID to restore to
-     *                                 (defaults to the backup's current
-     *                                 environment).
-     * @param string      $branchFrom  If a new environment is going to be
-     *                                 created via this action, specify the
-     *                                 parent branch for the new environment
-     *                                 (defaults to 'master').
-     *
-     * @return \Platformsh\Client\Model\Result
-     */
-    public function restore($environment = null, $branchFrom = null)
-    {
-        $options = [];
-        if ($environment !== null) {
-            $options['environment_name'] = $environment;
-        }
-        if ($branchFrom !== null) {
-            $options['branch_from'] = $branchFrom;
-        }
-
-        return $this->runOperation('restore', 'post', $options);
-    }
 }
