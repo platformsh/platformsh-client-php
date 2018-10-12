220947685624464455d8ae3022c418ae832ce85e
Patrick Dawkins <pjcdawkins@gmail.com>
2018-10-12 17:21:31 +0100

    Fix PHP <5.6 syntax error in Duration

--- src/Model/Type/Duration.php
+++ src/Model/Type/Duration.php
@@ -9,15 +9,7 @@ class Duration
 {
     private $seconds;
 
-    public static $suffixes = [
-        's' => 1,
-        'm' => 60,
-        'h' => 60 * 60,
-        'd' => 24 * 60 * 60,
-        'w' => 7 * 24 * 60 * 60,
-        'M' => 30 * 24 * 60 * 60,
-        'y' => 365 * 24 * 60 * 60,
-    ];
+    private static $suffixes;
 
     /**
      * @param int|string $duration
@@ -44,13 +36,38 @@ class Duration
      */
     public function __toString()
     {
-        foreach (array_reverse(self::$suffixes) as $suffix => $unit) {
+        foreach (array_reverse(self::suffixes()) as $suffix => $unit) {
             if ($this->seconds % $unit === 0) {
                 return sprintf('%s%s', $this->seconds / $unit, $suffix);
             }
         }
 
         return (string) $this->seconds;
+    }
+
+    /**
+     * List valid date/time suffixes.
+     *
+     * @todo define in the static variable declaration in PHP 5.6+, or in a private constant in PHP 7.1+
+     *
+     * @return array An array whose keys are suffixes and whose values are
+     * durations in seconds.
+     */
+    public static function suffixes()
+    {
+        if (isset(self::$suffixes)) {
+            return self::$suffixes;
+        }
+
+        return self::$suffixes = [
+            's' => 1,
+            'm' => 60,
+            'h' => 60 * 60,
+            'd' => 24 * 60 * 60,
+            'w' => 7 * 24 * 60 * 60,
+            'M' => 30 * 24 * 60 * 60,
+            'y' => 365 * 24 * 60 * 60,
+        ];
     }
 
     /**
@@ -62,9 +79,9 @@ class Duration
      */
     private static function stringToSeconds($duration)
     {
-        if (isset(self::$suffixes[substr($duration, -1)])) {
+        if (isset(self::suffixes()[substr($duration, -1)])) {
             $amount = substr($duration, 0, strlen($duration) - 1);
-            $unit = self::$suffixes[substr($duration, -1)];
+            $unit = self::suffixes()[substr($duration, -1)];
         } else {
             $unit = 1;
             $amount = $duration;
