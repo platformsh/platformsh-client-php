d2f9eb1a5fe550e3b07768e838c7992a22b31fce
Patrick Dawkins <pjcdawkins@gmail.com>
2020-10-25 13:15:44 +0000

    Document backup "restorable" property

--- src/Model/Backup.php
+++ src/Model/Backup.php
@@ -8,9 +8,9 @@ namespace Platformsh\Client\Model;
  * @property-read string $id          The backup name/ID.
  * @property-read string $status      The status of the backup.
  * @property-read int    $index       The index of this automated backup.
- * @property-read string $commit_id   The code commit ID attached to this
- *                backup.
+ * @property-read string $commit_id   The code commit ID attached to this backup.
  * @property-read string $environment The environment the backup belongs to.
+ * @property-read bool   $restorable   Whether the backup is restorable.
  * @property-read string $created_at
  * @property-read string $updated_at
  * @property-read string $expires_at
