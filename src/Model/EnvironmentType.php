9ebd0a1c0c44a30944e889a2029aac8d009a186b
Patrick Dawkins <pjcdawkins@gmail.com>
2021-06-15 10:26:35 +0100

    Add EnvironmentTypeAccess class

--- src/Model/EnvironmentType.php
+++ src/Model/EnvironmentType.php
@@ -6,4 +6,40 @@ namespace Platformsh\Client\Model;
  * @property-read string $id
  * @property-read array  $attributes
  */
-class EnvironmentType extends Resource {}
+class EnvironmentType extends Resource
+{
+    /**
+     * Add a user to this environment type.
+     *
+     * @param string $id
+     * @param string $role
+     *
+     * @return Result
+     */
+    public function addUser($id, $role)
+    {
+        return EnvironmentTypeAccess::create(['user' => $id, 'role' => $role], $this->getLink('#access'), $this->client);
+    }
+
+    /**
+     * Get a user's access to this environment type.
+     *
+     * @param string $uuid
+     *
+     * @return EnvironmentTypeAccess|false
+     */
+    public function getUser($uuid)
+    {
+        return EnvironmentTypeAccess::get($uuid, $this->getLink('#access'), $this->client);
+    }
+
+    /**
+     * Get the users with access to this environment type.
+     *
+     * @return EnvironmentTypeAccess[]
+     */
+    public function getUsers()
+    {
+        return EnvironmentTypeAccess::getCollection($this->getLink('#access'), 0, [], $this->client);
+    }
+}
