22d3523bb3961b92a034e727363463dde7398581
Patrick Dawkins <pjcdawkins@gmail.com>
2021-08-18 09:58:24 +0100

    Implement updating an organization

--- src/Model/Organization/Organization.php
+++ src/Model/Organization/Organization.php
@@ -9,6 +9,7 @@ use Platformsh\Client\Model\Organization\Invitation\OrganizationInvitation;
 use Platformsh\Client\Model\Ref\UserRef;
 use Platformsh\Client\Model\ResourceWithReferences;
 use Platformsh\Client\Model\Subscription;
+use Platformsh\Client\Model\Result;
 
 /**
  * @property-read string $id
@@ -27,6 +28,30 @@ class Organization extends ResourceWithReferences
             return $this->getUri($absolute) . '/invitations';
         }
         return parent::getLink($rel, $absolute);
+    }
+
+    /**
+     * Updates the organization.
+     *
+     * This updates the resource's internal data with the API response.
+     *
+     * @param array $values
+     *
+     * @return Result
+     */
+    public function update(array $values)
+    {
+        // @todo use getLink('#edit') when it is available
+        $url = $this->getUri();
+        $options = [];
+        if (!empty($values)) {
+            $options['json'] = $values;
+        }
+        $response = $this->client->patch($url, $options);
+        $data = $response->json();
+        $this->setData($data);
+
+        return new Result($data, $this->baseUrl, $this->client, get_called_class());
     }
 
     /**
