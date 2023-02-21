f3f90925d3fa15dded2a546f1e9a5dda56134e02
Patrick Dawkins <patrick@platform.sh>
2023-02-21 20:45:25 +0000

    Fix return type for getProjectInfo() on UserProjectAccess

--- src/Model/Ref/ProjectRef.php
+++ src/Model/Ref/ProjectRef.php
@@ -7,10 +7,8 @@ use Platformsh\Client\DataStructure\ReadOnlyStructureTrait;
 /**
  * @property-read string $id
  * @property-read string $organization_id
- * @property-read string $subscription_id
  * @property-read string $region
  * @property-read string $title
- * @property-read string $status
  * @property-read string $created_at
  * @property-read string $updated_at
  */
