7a959b7b929646318325b7c351f2c5e4ead60010
Patrick Dawkins <patrick@platform.sh>
2023-03-20 11:23:41 +0000

    Add Environment::getBackup() method

--- src/Model/Environment.php
+++ src/Model/Environment.php
@@ -654,7 +654,17 @@ class Environment extends ApiResourceBase implements HasActivitiesInterface
     }
 
     /**
-     * Get a list of environment backups.
+     * Fetches a single environment backup.
+     *
+     * @return Backup|false
+     */
+    public function getBackup($id)
+    {
+        return Backup::get($id, $this->getUri() . '/backups', $this->client);
+    }
+
+    /**
+     * Fetches a list of environment backups.
      *
      * @param int $limit
      *   Limit the number of backups to return.
