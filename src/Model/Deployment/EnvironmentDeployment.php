9e8a8f071eedeea47049bc247d596a8bcf822d5a
Florian Margaine <ralt@users.noreply.github.com>
2026-03-02 12:10:54 +0100

    Apply suggestions from code review
    Co-authored-by: Patrick Dawkins <pjcdawkins@users.noreply.github.com>

--- src/Model/Deployment/EnvironmentDeployment.php
+++ src/Model/Deployment/EnvironmentDeployment.php
@@ -100,7 +100,7 @@ class EnvironmentDeployment extends ApiResourceBase
      *   The operation name.
      * @param string $service
      *   The name of the service or application to run the operation on.
-     * @param array<string, string> $parameters
+     * @param string[] $parameters
      *   Parameters to pass to the operation, as key-value pairs.
      *
      *@see RuntimeOperation
